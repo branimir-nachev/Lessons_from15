cd3de2828e15b8209110020f473d9d86d74707e8
Branimir Nachev <32915919+branimir-nachev@users.noreply.github.com>
2022-08-03 19:09:31 +0300

    Add Customer timer Interupt

--- Lessons_from15/bsp.h
+++ Lessons_from15/bsp.h
@@ -1,6 +1,13 @@
 #ifndef __DELAY_H__
 #define __DELAY_H__
 
+#define SYS_CLOCK_HZ 16000000U
+
+#define LED_RED   (1U << 1)
+#define LED_BLUE  (1U << 2)
+#define LED_GREEN (1U << 3)
+
+
 void delay(int volatile iter);
 
 #endif // __DELAY_H__
